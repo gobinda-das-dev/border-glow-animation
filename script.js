a134f11bb90052073121bba93c9aa5e64b203f64
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-06-08 17:10:35 +0530

    do not rotate instead use the angle in the mask image

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const parent = document.querySelector('.parent');
 const mask = parent.querySelector('.mask-gradient');
 
 const tw = gsap.to(mask, {
-   maskImage: 'linear-gradient(to right, transparent -80%, black 0%)',
+   '--p1': '-80%',
+   '--p2': '0%',
    duration: 0.3,
    ease: 'power1.inOut',
    paused: true,
@@ -21,7 +22,7 @@ const size = { width: 0, height: 0 };
 const mouse = { x: 0, y: 0 };
 let getOpacity;
 
-const setRotate = gsap.quickSetter(mask, 'rotate', 'rad');
+const setRotate = gsap.quickSetter(mask, '--angle', 'rad');
 const setOpacity = gsap.quickSetter(mask, 'opacity');
 
 const calculatePosition = () => {
@@ -29,26 +30,26 @@ const calculatePosition = () => {
    position.x = x + width / 2;
    position.y = y + height / 2;
    size.width = width;
-   size.height = height;console.log(y);
+   size.height = height; console.log(y);
 
    getOpacity = gsap.utils.pipe(
-      gsap.utils.mapRange(0, 1, 1 + width/respectedBound, 0),
+      gsap.utils.mapRange(0, 1, 1 + width / respectedBound, 0),
       gsap.utils.clamp(0, 1)
    );
 }
 
-const updateMousePosition = (e) => {   
+const updateMousePosition = (e) => {
    mouse.x = e.clientX;
    mouse.y = e.clientY;
    const distance = Math.sqrt((mouse.x - position.x) ** 2 + (mouse.y - position.y) ** 2);
-   if(distance > (respectedBound + size.width)) return;
-   
+   if (distance > (respectedBound + size.width)) return;
+
    const angle = Math.atan2(mouse.y - position.y, mouse.x - position.x);
    const normalizedDistance = distance / respectedBound;
-   
+
    const opacity = getOpacity(normalizedDistance);
 
-   setRotate(angle);
+   setRotate(angle + Math.PI / 2);
    setOpacity(opacity);
 }
 
